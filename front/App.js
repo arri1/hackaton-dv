e61bb08ee690a029bea8bc890cad6fc03c6f331f
arri <ewws@mail.ru>
2020-10-03 04:31:54 +0900

    fixes app.js and add prettierrc

--- front/App.js
+++ front/App.js
@@ -1,21 +1,13 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react'
+import { Text, View } from 'react-native'
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+const App = () => {
+    return (
+        <View >
+            <Text>Open up App.js to start working on your app!</Text>
+        </View>
+    )
 }
+export default App
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+
