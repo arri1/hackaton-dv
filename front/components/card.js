fe2105e70670d184edf40a0af5a31448b7a04522
Ioan Okoneshnikov <golden9208@gmail.com>
2020-10-03 15:05:33 +0900

    asdokasd

--- front/components/card.js
+++ front/components/card.js
@@ -6,7 +6,10 @@ const Old = styled(View)`
     flex-direction: column;
     background-color: white;
     border-radius: 15px;
-    margin:10px 15px;
+    margin-top: 5px;
+    margin-bottom: 5px;
+    margin-left:15px;
+    margin-right:15px;
 `
 const Row = styled(View)`
     display: flex;
@@ -15,18 +18,18 @@ const Row = styled(View)`
     margin-bottom: 10px;
 `
 const Padding = styled(View)`
-    padding:10px;
+    padding: 10px;
 `
 const Card = (props) => {
     const { name, description, type } = props
     return (
         <Old>
             <Padding>
-            <Row>
-                <Text>{name}</Text>
-                <Text>{type}</Text>
-            </Row>
-            <Text>{description}</Text>
+                <Row>
+                    <Text>{name}</Text>
+                    <Text>{type}</Text>
+                </Row>
+                <Text>{description}</Text>
             </Padding>
         </Old>
     )
--- front/screens/marketDetail.js
+++ front/screens/marketDetail.js
@@ -1,17 +1,45 @@
 import React from 'react'
-import { Text, View } from 'react-native'
+import { Text, View, Image} from 'react-native'
 import styled from 'styled-components'
 
-const Container= styled(View)`
-  flex:1;
-  align-items: center;
-  justify-content: center;
+const Container = styled(View)`
+    display: flex;
+    flex-direction: column;
+    border-radius: 15px;
+    margin-top: 20px;
+    margin-bottom: 5px;
+    margin-left: 15px;
+    margin-right: 15px;
+`
+const Up = styled(View)`
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    margin-bottom: 10px;
+`
+const Font = styled(Text)`
+    font-weight: bold;
+    font-size: 18px;
+`
+const OFont = styled(Text)`
+    font-size: 18px;
+    color: #EE8F01;
+`
+const Imga = styled(View)`
+    display:flex;
+    flex:1;
+    height:50;
+    width:50;
 `
 
 const MarketDetail = () => {
     return (
         <Container>
-            <Text>MarketDetail</Text>
+            <Up>
+                <Font>Пятерочка</Font>
+                <OFont>Продукты</OFont>
+            </Up>
+            <Image style={Imga} sourse={{uri:'https://dev-gang.ru/static/storage/24808028622541768947383165896446886988.png'}}/>
         </Container>
     )
 }
