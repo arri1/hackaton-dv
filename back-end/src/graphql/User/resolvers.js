ada0179aab0ba2aa3e1cb8b6284d553ac60e0894
OctoMammoth <37862408+OctoMammoth@users.noreply.github.com>
2020-10-04 00:53:23 +0900

    Uncommit user auth and reg

--- back-end/src/graphql/User/resolvers.js
+++ back-end/src/graphql/User/resolvers.js
@@ -19,28 +19,28 @@ const User = {
     },
   },
   Mutation: {
-    // register: async (_parent, {data}, { prisma }) => {
-    //   const {password} = data
-    //   passwordBcrypt = await bcrypt.hash(password, 10)
-    //   data.password = passwordBcrypt
-    //   const user = await prisma.user.create({data})
-    //   const token = jwt.sign({id: user.id}, process.env.USER_SECRET)
-    //   return {
-    //     user,
-    //     token
-    //   }
-    // },
-    // authUser: async (_parent, {data}, { prisma }) => {
-    //   const {password, login} = data
-    //   const user = await prisma.user.findOne({where: {login}})
-    //   const compare = bcrypt.compareSync(password, business.compare)
-    //   if (!compare) throw new Error("Incorrect password")
-    //   const token = await jwt.sign({id: user.id}, process.env.USER_SECRET)
-    //   return {
-    //     token,
-    //     user
-    //   }
-    // },
+     registerUser: async (_parent, {data}, { prisma }) => {
+       const {password} = data
+       passwordBcrypt = await bcrypt.hash(password, 10)
+       data.password = passwordBcrypt
+       const user = await prisma.user.create({data})
+       const token = jwt.sign({id: user.id}, process.env.USER_SECRET)
+       return {
+         user,
+         token
+       }
+     },
+     authUser: async (_parent, {data}, { prisma }) => {
+       const {password, login} = data
+       const user = await prisma.user.findOne({where: {login}})
+       const compare = bcrypt.compareSync(password, business.compare)
+       if (!compare) throw new Error("Incorrect password")
+       const token = await jwt.sign({id: user.id}, process.env.USER_SECRET)
+       return {
+         token,
+         user
+       }
+     },
     createOneUser: (_parent, args, { prisma }) => {
       return prisma.user.create(args)
     },
