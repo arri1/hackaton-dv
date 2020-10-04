6cd318c5a4e7f9ef47e0d4286533d1b9f9e95858
arri <ewws@mail.ru>
2020-10-04 11:40:28 +0900

    fix useState

--- front/screens/markets.js
+++ front/screens/markets.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React,{useState} from 'react'
 import styled from 'styled-components'
 import {useQuery} from '@apollo/react-hooks'
 import {ScrollView, TouchableOpacity, View} from 'react-native'
